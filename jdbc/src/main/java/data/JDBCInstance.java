a8160114eee3d0b3c532748e0e7ce3605247f371
doquanganh99zz <do.quanganh99zz@gmail.com>
2023-12-31 00:05:13 +0700

    update

--- jdbc/src/main/java/data/JDBCInstance.java
+++ jdbc/src/main/java/data/JDBCInstance.java
@@ -1,7 +1,38 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCInstance {
-    public static void getInstance(){
-        // logic
+
+    public static final String HOSTNAME = "localhost";
+    public static final String PORT = "3306";
+    public static final String DBNAME = "test1";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "mvmvmv99";
+
+    /**
+     * Get connection to MSSQL Server
+     *
+     * @return Connection
+     */
+    public static Connection getConnection() {
+
+        // Create a variable for the connection string.
+        String connectionUrl = "jdbc:mysql://" + HOSTNAME + ":" + PORT + "/"
+               + DBNAME;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(connectionUrl, USERNAME, PASSWORD);
+        } // Handle any errors that may have occurred.
+        catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace(System.out);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        getConnection();
     }
 }
